ae3802e262ce23af332c906e84be4028137c62f7
suman520-git <svemula520@gmail.com>
2025-06-30 22:43:24 +0530

    Data ingestion completed

---
+++ main.py
@@ -0,0 +1,14 @@
+from src.TextSummarization.logging import logger
+from src.TextSummarization.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
+
+
+STAGE_NAME="Data Ingestion stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    data_ingestion_pipeline=DataIngestionTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e

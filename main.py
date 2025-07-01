377b420e6d13d3888f10aae5323bfc8fb84f5c48
suman520-git <svemula520@gmail.com>
2025-07-01 13:13:46 +0530

    Data transformation done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.TextSummarization.logging import logger
 from src.TextSummarization.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.TextSummarization.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
 
 
 STAGE_NAME="Data Ingestion stage"
@@ -12,3 +13,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME="Data Transformation stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    data_ingestion_pipeline=DataTransformationTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_transformation()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e    
